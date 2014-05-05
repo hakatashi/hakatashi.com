a58f51c79fdc437a60bbbc48ee34f753a104b819
hakatashi <hakatasiloving@gmail.com>
2014-05-05 18:02:11 +0900

    Kick index.html off

---
+++ index.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    var viewBox = {
+        height: 720,
+        width: 1080
+    };
+    var paper = Raphael("wrap");
+    paper.setViewBox(0, 0, viewBox.width, viewBox.height, true);
+
+    // http://stackoverflow.com/questions/11176396/
+    var svg = document.querySelector("svg");
+    svg.removeAttribute("width");
+    svg.removeAttribute("height");
+
+    paper.circle(
+        540,
+        360,
+        200
+    ).attr({
+        fill: "black",
+        opacity: 0.5
+    });
+});
